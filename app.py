69c4b3da37794cbdb92cbb59a7b92a2c1a6e6e27
David Paner <panerdavid@Davids-MacBook-Pro.local>
2019-10-13 10:59:27 -0700

    Hi Marvin :)

---
+++ app.py
@@ -0,0 +1,65 @@
+from eventbrite import Eventbrite
+from tok import secret
+import pprint
+
+
+token = secret()
+eventbrite = Eventbrite(token)
+pp = pprint.PrettyPrinter(indent=4)
+user = eventbrite.get_user()
+
+# name, url, description, date,  image, *location (can be found in the expansion of venue)
+
+
+def eb_api_query():
+    # dictionary keys of argument are the fields we care about
+    arguments = {"location.address":"", "categories": "", "start_date.range_start" : ""}
+
+    # do something like this...
+    arguments["location.address"] = "Berkeley"
+    # YYYY-MM-DDThh:mm:ssZ
+    arguments["start_date.range_start"] = "2019-09-29T00:00:00"
+    arguments["categories"] = "103"
+    
+
+
+
+    #this line is to expand the venue field (necessary to retrieve address)
+    arguments["expand"] = "venue"
+
+    results = eventbrite.event_search(**arguments)
+    d = {}
+
+    for i in range(len(results["events"])):
+        name = (results["events"][i]["name"]["text"])
+        sub_d = {}
+        sub_d['url'] = results["events"][i]["url"]
+        sub_d['description'] = results["events"][i]["description"]["text"]
+        sub_d['date'] = results["events"][i]["start"]["local"]
+        sub_d['image'] = results["events"][i]["logo"]["original"]["url"]        
+        sub_d['location'] = results["events"][i]["venue"]["address"]["localized_multi_line_address_display"]        
+        d[name] = sub_d.copy()
+    return d
+        # pp.pprint(results["events"][i]["url"])
+        # pp.pprint(results["events"][i]["description"]["text"])
+        # pp.pprint(results["events"][i]["start"]["local"])
+        # pp.pprint(results["events"][i]["logo"]["original"]["url"])
+        # pp.pprint(results["events"][i]["venue"]["address"]["localized_multi_line_address_display"])
+
+
+
+
+   
+   
+    #populate this dictionary with info from the search
+
+
+    #name, url, description, date,  image, *location
+  
+
+   
+
+
+    
+
+eb_api_query()
